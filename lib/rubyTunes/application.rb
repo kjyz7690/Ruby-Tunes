f90a4fe326a56e69c4829a385d1a6b3a77dc640d
kate young <katherine.young@cerner.com>
2015-05-30 14:52:33 -0500

    Initialize iTunes library hash'

--- lib/rubyTunes/application.rb
+++ lib/rubyTunes/application.rb
@@ -3,10 +3,20 @@ require 'xmlsimple'
 class Application
   def initialize(path)
     @path = path
+    self.savePath()
     filePath = @path + '/' + 'iTunes Music Library.xml'
     @config = XmlSimple.xml_in(filePath, {'KeyAttr' => 'Track ID'} )
+    self.createSongHash()
   end
-  private def createSongHash()
-    File.open('iTunesLib.rb')
+  def createSongHash()
+      File.open('iTunesLib.rb','w') do  |itunes|
+        itunes.truncate(0)
+        itunes.puts @config
+      end
+  end
+  def savePath()
+    File.open('config.rb','w') do |config|
+      config.puts 'path = "' + @path + '"'
+    end
   end
 end
